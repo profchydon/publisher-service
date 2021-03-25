7e5f8cc6ebded71bee6d8b35f695773f93000b50
Chidi <profchydon@gmail.com>
2021-03-25 21:41:30 +0100

    Subscribe servers to topics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CARS_SUBSCRIBER',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'cars',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+      {
+        name: 'PHONE_SUBSCRIBER',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'phones',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
